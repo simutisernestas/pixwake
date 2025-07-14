3b3fe74c533ab292fc022401d9f342e145db384b
ernie <ernim@dtu.dk>
2025-07-14 13:25:37 +0200

    surrogate scaling

--- test/test_rans_surrogate.py
+++ test/test_rans_surrogate.py
@@ -4,38 +4,91 @@ import jax.numpy as jnp
 import numpy as np
 from flax import serialization
 import time
+from numpy import newaxis as na
 
 
 class WakeDeficitModelFlax(fnn.Module):
+    scale_x = jnp.array(
+        [21.21759238, 3.60546819, 0.31714823, 0.09218609, 18.70851079, 0.25810896]
+    )
+    mean_x = jnp.array(
+        [
+            3.34995157e1,
+            3.63567130e-04,
+            2.25024289e-02,
+            1.43747711e-01,
+            -1.45229452e-03,
+            6.07149107e-01,
+        ]
+    )
+    scale_y = jnp.array([0.02168894])
+    mean_y = jnp.array([0.00614207])
+
     @fnn.compact
     def __call__(self, x):
+        x = (x - self.mean_x) / self.scale_x
         x = fnn.tanh(fnn.Dense(70)(x))
         x = fnn.sigmoid(fnn.Dense(102)(x))
         x = fnn.sigmoid(fnn.Dense(102)(x))
         x = fnn.sigmoid(fnn.Dense(102)(x))
         x = fnn.Dense(1)(x)
-        return x
+        return (x * self.scale_y) + self.mean_y
 
 
 class WakeAddedTIModelFlax(fnn.Module):
+    scale_x = jnp.array(
+        [21.21759238, 3.60546819, 0.31714823, 0.09218609, 18.70851079, 0.25810896]
+    )
+    mean_x = jnp.array(
+        [
+            3.34995157e1,
+            3.63567130e-04,
+            2.25024289e-02,
+            1.43747711e-01,
+            -1.45229452e-03,
+            6.07149107e-01,
+        ]
+    )
+    scale_y = jnp.array([0.00571155])
+    mean_y = jnp.array([0.0014295])
+
     @fnn.compact
     def __call__(self, x):
+        x = (x - self.mean_x) / self.scale_x
         x = fnn.sigmoid(fnn.Dense(118)(x))
         x = fnn.sigmoid(fnn.Dense(118)(x))
         x = fnn.sigmoid(fnn.Dense(118)(x))
         x = fnn.sigmoid(fnn.Dense(118)(x))
         x = fnn.Dense(1)(x)
-        return x
+        return (x * self.scale_y) + self.mean_y
+
+
+# def get_input_from_pywake(
+#     dw_ijlk,
+#     hcw_ijlk,
+#     z_ijlk,
+#     ct_ilk,
+#     TI_ilk,
+#     h_ilk,
+#     D_src_il,
+#     TI_eff_ilk,
+#     use_eff_ti=False,
+#     **kwargs,
+# ):
+#     x, y = dw_ijlk / D_src_il[:, na, :, na], hcw_ijlk / D_src_il[:, na, :, na]
+#     z = (z_ijlk - h_ilk[:, na]) / D_src_il[:, na, :, na]
+#     ti = TI_eff_ilk if use_eff_ti else TI_ilk
+#     return x, y, z, ti, np.zeros_like(ct_ilk), ct_ilk
 
 
 if __name__ == "__main__":
     input_dim = 6
-    model = WakeDeficitModelFlax()
+    model = WakeAddedTIModelFlax()
     dummy_input = jnp.ones((1, input_dim))
     variables = model.init(jax.random.PRNGKey(0), dummy_input)
 
     # Load the parameters
-    with open("./data/rans_deficit_surrogate.msgpack", "rb") as f:
+    with open("./data/rans_addedti_surrogate.msgpack", "rb") as f:
         bytes_data = f.read()
     restored_variables = serialization.from_bytes(variables, bytes_data)
 
@@ -50,7 +103,7 @@ if __name__ == "__main__":
 
     # Warmup (compile)
     output = value_and_grad_func(restored_variables, real_input_jax)
-    # print("Model output:", output)
+    print("Model output:", output)
 
     # time it with large batch size
     batch_size = 10_000
