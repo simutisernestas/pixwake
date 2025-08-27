5da006f36a416b54342d5f2f72f0912ce5b0e581
ernie <ernim@dtu.dk>
2025-08-27 13:38:55 +0200

    switch np.floating to jnp.floating

--- src/pixwake/utils.py
+++ src/pixwake/utils.py
@@ -1,9 +1,8 @@
 import jax.numpy as jnp
 from jax import config as jcfg
-from numpy import floating
 
 
-def get_eps() -> floating:
+def get_eps() -> jnp.floating:
     """Returns the machine epsilon for the current JAX float precision."""
     return (
         jnp.finfo(jnp.float64).eps
